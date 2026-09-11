329c3d4cbeb7bc92852acfd1f11ffb477e2a2166
Ali Mousavi <ali.mousavi@gmail.com>
2026-09-11 19:40:24 +0330

    chore: enable GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/chessnote/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
